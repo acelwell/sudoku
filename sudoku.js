57b2e3f502518f805f29686f5a5dbb9776001ab7
acelwell <krytality@hotmail.com>
2020-06-07 02:11:25 -0700

    completed

--- sudoku.js
+++ sudoku.js
@@ -1,6 +1,10 @@
 let constBoard = new Array(9);
 let playBoard = new Array(9);
 
+let shortDelay = 17;
+let delay = 77;
+let longDelay = 200;
+
 let r1 = "586374912";
 let r2 = "137952864";
 let r3 = "249816573";
@@ -14,17 +18,29 @@ let r9 = "368195427";
 // valid for all possibilites
 
 
-// let r1 = "586374912";
+// let r1 = "556374912";
 // let r2 = "137952864";
 // let r3 = "249816573";
 // let r4 = "872543196";
-// let r5 = "693789245";
+// let r5 = "693781245";
 // let r6 = "415629738";
 // let r7 = "954237681";
 // let r8 = "721468359";
 // let r9 = "368195427";
 
 let rows = [r1, r2, r3, r4, r5, r6, r7, r8, r9]
+
+let colorBoard = [
+    [1,1,1,1,1,1,1,1,1],
+    [1,1,1,1,1,1,1,1,1],
+    [1,1,1,1,1,1,1,1,1],
+    [1,1,1,1,1,1,1,1,1],
+    [1,1,1,1,1,1,1,1,1],
+    [1,1,1,1,1,1,1,1,1],
+    [1,1,1,1,1,1,1,1,1],
+    [1,1,1,1,1,1,1,1,1],
+    [1,1,1,1,1,1,1,1,1]
+]
 
 function initializeConstBoard ()
 {
@@ -46,58 +62,152 @@ function initializeConstBoard ()
     console.log("initialized board");
 }
 
-function validSudoku()
+async function validSudoku()
 {
-    let rowHashSet = new Set();
-    let colHashSet = new Set();
+    if(document.getElementById("rows").checked == true)
+    {
+        await validRows();
+    }
+    if(document.getElementById("columns").checked == true)
+    {
+        await validCols();
+    }
+    if(document.getElementById("boxes").checked == true)
+    {
+        await validBoxes();
+    }
+    if(document.getElementById("knightsMove").checked == true)
+    {
+        await validKnightsMove();
+    }
+    if(document.getElementById("kingsMove").checked == true)
+    {
+        await validKingsMove();
+    }
+    if(document.getElementById("cross").checked == true)
+    {
+        await validCross();
+    }
+
+
+
+    updateColorBoard();
     
+}
+
+async function validRows()
+{
+    
+    let valid = true;
 
     for(let i = 0; i < 9; i++)
     {
+        let rowHashSet = new Set();
+
         for(let k = 0; k < 9; k++)
         {
+            let square = document.getElementById((9 * i + k + 1).toString());
+            square.style.backgroundColor = "#ffff00";
             if(constBoard[i][k] >= 1 && constBoard[i][k] <= 9)
             {
                 rowHashSet.add(constBoard[i][k]);
             }
+            await sleep(delay);
+            // console.log("why arent we coloring??");
+            
+        }
+
+        if(rowHashSet.size != 9)
+        {
+            // console.log("not a valid sudoku");
+            valid = false;
+            colorRow(i, "red");
+            updateColorBoardRow(i, 0);
+        }
+        else
+        {
+            colorRow(i, "green");
+        }
+
+        await sleep(longDelay);
+        colorRow(i, "#aaaaaa");
+    }
+
+    return valid;
+}
+
+async function validCols()
+{
+    let valid = true;
+    for(let i = 0; i < 9; i++)
+    {
+
+        let colHashSet = new Set();
+        for(let k = 0; k < 9; k++)
+        {
+            let square = document.getElementById((9 * k + i + 1));
+            square.style.backgroundColor = "#ffff00";
             if(constBoard[k][i] >= 1 && constBoard[k][i] <= 9)
             {
                 colHashSet.add(constBoard[k][i]);
             }
+            await sleep (delay);
         }
 
-        if(rowHashSet.size != 9 || colHashSet.size != 9)
+        if(colHashSet.size != 9)
         {
             console.log("not a valid sudoku");
-            return;
+            valid = false;
+            colorCol(i, "red");
+            updateColorBoardCol(i, 0);
         }
+        else
+        {
+            colorCol(i, "green");
+        }
+
+        await sleep (longDelay);
+
+        colorCol(i, "#aaaaaa");
+
+        
     }
 
+    return valid;
+}
+
+async function validBoxes()
+{
+    let valid = true;
     for(let i = 0; i < 9; i += 3)
     {
         for(let k = 0; k < 9; k += 3)
         {
-            if(!checkBox(i,k))
-            {
-                console.log("not valid sudoku");
-                return;
-            }
+            // if(!checkBox(i,k))
+            // {
+            //     console.log("not valid sudoku");
+            //     return false;
+            // }
+            valid = valid && checkBox(i, k);
         }
     }
 
-    console.log("valid sudoku yay!");
-    
+    return valid;
 }
 
-function checkBox(row, col)
-{
 
+async function checkBox(row, col)
+{
+    let valid = true;
     let boxHashSet = new Set();
 
     for(let i = row; i < row + 3; i++)
     {
         for(let k = col; k < col + 3; k++)
         {
+            let square = document.getElementById((9 * i + k + 1).toString());
+            square.style.backgroundColor = "#ffff00";
+            await sleep(delay);
             if(constBoard[i][k] >= 1 && constBoard[i][k] <= 9)
             {
                 boxHashSet.add(constBoard[i][k]);
@@ -105,21 +215,51 @@ function checkBox(row, col)
         }
     }
 
-    return boxHashSet.size == 9;
+    if(boxHashSet.size != 9)
+    {
+        valid = false;
+        colorBox(row, col, "red");
+        updateColorBoardBox(row, col, 0);
+    }
+    else
+    {
+        colorBox(row, col, "green");
+    }
+
+    await sleep(longDelay);
+    colorBox(row, col, "#aaaaaa");
+
+    return valid;
 }
 
-function checkValidNumber(num, row, col)
+async function checkValidNumber(num, row, col)
 {
     if(row < 0 || row > 8 || col < 0 || col > 8)
     {
         return true;
     }
 
-    return num != constBoard[row][col];
+    let valid = num != constBoard[row][col];
+    let square = document.getElementById((9 * row + col + 1));
+    if(valid)
+    {
+        square.style.backgroundColor = "green";
+    }
+    else
+    {
+        square.style.backgroundColor = "red";
+        updateColorBoardCell(row, col, 0);
+    }
+
+    await sleep(delay);
+
+    square.style.backgroundColor = "#aaaaaa";
+
+    return valid;
 
 }
 
-function validKnightsMove()
+async function validKnightsMove()
 {
     let result = true;
 
@@ -127,29 +267,38 @@ function validKnightsMove()
     {
         for(let k = 0; k < 9; k++)
         {
-            result = result && checkValidNumber(constBoard[i][k], i - 2, k - 1);
-            result = result && checkValidNumber(constBoard[i][k], i - 2, k + 1);
-            result = result && checkValidNumber(constBoard[i][k], i + 2, k - 1);
-            result = result && checkValidNumber(constBoard[i][k], i + 2, k + 1);
+            let square = document.getElementById((9 * i + k + 1));
+            square.style.backgroundColor = "#ffff00";
+            
 
-            result = result && checkValidNumber(constBoard[i][k], i - 1, k - 2);
-            result = result && checkValidNumber(constBoard[i][k], i - 1, k + 2);
-            result = result && checkValidNumber(constBoard[i][k], i + 1, k - 2);
-            result = result && checkValidNumber(constBoard[i][k], i + 1, k + 2);
+            result = result &&  checkValidNumber(constBoard[i][k], i - 2, k - 1);
+            result = result &&  checkValidNumber(constBoard[i][k], i - 2, k + 1);
+            result = result &&  checkValidNumber(constBoard[i][k], i + 2, k - 1);
+            result = result &&  checkValidNumber(constBoard[i][k], i + 2, k + 1);
+
+            result = result &&  checkValidNumber(constBoard[i][k], i - 1, k - 2);
+            result = result &&  checkValidNumber(constBoard[i][k], i - 1, k + 2);
+            result = result &&  checkValidNumber(constBoard[i][k], i + 1, k - 2);
+            result = result &&  checkValidNumber(constBoard[i][k], i + 1, k + 2);
+
+            await sleep(delay);
+
+            square.style.backgroundColor = "#aaaaaa";
+
+            
 
             if(!result)
             {
                 console.log("knights move failed at cell: " + i + " " + k);
-                
-                return;
             }
         }
     }
 
+    return result;
 
 }
 
-function validKingsMove()
+async function validKingsMove()
 {
     let result = true;
 
@@ -157,6 +306,9 @@ function validKingsMove()
     {
         for(let k = 0; k < 9; k++)
         {
+            let square = document.getElementById((9 * i + k + 1).toString());
+            square.style.backgroundColor = "#ffff00";
+
             result = result && checkValidNumber(constBoard[i][k], i - 1, k - 1);
             result = result && checkValidNumber(constBoard[i][k], i - 1, k);
             result = result && checkValidNumber(constBoard[i][k], i - 1, k + 1);
@@ -168,6 +320,9 @@ function validKingsMove()
             result = result && checkValidNumber(constBoard[i][k], i + 1, k);
             result = result && checkValidNumber(constBoard[i][k], i + 1, k + 1);
 
+            await sleep(delay);
+            square.style.backgroundColor = "#aaaaaa";
+
             if(!result)
             {
                 console.log("kings move failed at cell: " + i + " " + k);
@@ -178,13 +333,19 @@ function validKingsMove()
     }
 }
 
-function validCross()
+async function validCross()
 {
+    let valid = true;
     let c1 = new Set();
     let c2 = new Set();
 
     for(let i = 0; i < 9; i++ )
     {
+        let square1 = document.getElementById((9 * i + i + 1));
+        square1.style.backgroundColor = "#ffff00";
+
+        let square2 = document.getElementById(((8 - i) * 9 + i + 1 ).toString());
+        square2.style.backgroundColor = "#ffff00";
         if(constBoard[i][i] >= 1 && constBoard[i][i] <= 9)
         {
             c1.add(constBoard[i][i]);
@@ -194,17 +355,26 @@ function validCross()
         {
             c2.add(constBoard[8 - i][i]);
         }
+        await sleep(delay);
     }
 
     if(c1.size != 9 || c2.size != 9)
     {
         console.log("failed cross :(");
+        colorCross("red");
+        valid = false;
+        updateColorBoardCross(0);
     }
     else
     {
         console.log("passed cross :)");
-        
+        colorCross("green");
     }
+
+    await sleep(longDelay);
+    colorCross("#aaaaaa");
+
+    return valid;
 
 
 }
@@ -215,7 +385,7 @@ function createBoard()
     // board.style.justifyContent = 'center';
     
 
-    console.log(board);
+    // console.log(board);
     
 
     for(let i = 0; i < 9; i++)
@@ -225,7 +395,7 @@ function createBoard()
         row.style.display = "flex";
         row.style.flexDirection = 'row';
         row.style.justifyContent = 'center';
-        console.log(row);
+        // console.log(row);
         
 
         for(k = 0; k < 9; k++)
@@ -233,15 +403,16 @@ function createBoard()
             let square = document.createElement('div');
             square.className = 'square';
             square.style.display = 'flex';
-            square.style.backgroundColor = 'aqua';
+            square.style.backgroundColor = '#aaaaaa';
             square.style.height = '42px';
             square.style.width = '42px';
-            square.style.borderStyle = "dashed"
+            square.style.borderStyle = "solid"
             square.style.borderColor = 'black';
-            square.style.borderWidth = '1px';
+            square.style.borderWidth = '2px';
             square.style.justifyContent = 'center';
             square.style.alignItems = 'center';
-            square.id = k.toString();
+            square.style.fontSize = "40px";
+            square.id = (9 * i + k + 1).toString();
             square.append(constBoard[i][k]);
             row.appendChild(square);
         }
@@ -249,11 +420,153 @@ function createBoard()
         board.appendChild(row);
     }
 
-    // board.style.borderStyle = "solid";
-    // board.style.borderColor = "black";
-    // board.style.borderWidth = '1px;'
-
     console.log("board created");
+    
+}
+
+function sleep (ms)
+{
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function colorRow(row, color)
+{
+    let squares = document.getElementsByClassName("row")[row].children;
+
+    for (const square of squares) 
+    {
+        square.style.backgroundColor = color;   
+    }
+    
+
+}
+
+function colorCol(col, color)
+{
+    for(let i = 0; i < 9; i++)
+    {
+        let square = document.getElementById((9 * i + col + 1));
+        square.style.backgroundColor = color;
+    }
+}
+
+function colorBox(startRow, startCol, color)
+{
+    for(let i = startRow; i < startRow + 3; i++)
+    {
+        for(let k = startCol; k < startCol + 3; k++)
+        {
+            let square = document.getElementById((9 * i + k + 1).toString());
+            square.style.backgroundColor = color;
+        }
+    }
+}
+
+function colorCross(color)
+{
+    for(let i = 0; i < 9; i++ )
+    {
+        let square1 = document.getElementById((9 * i + i + 1));
+        square1.style.backgroundColor = color;
+
+        let square2 = document.getElementById(((8 - i) * 9 + i + 1 ).toString());
+        square2.style.backgroundColor = color;
+
+    }
+}
+
+function updateColorBoardRow(row, valid)
+{
+    for(let i =0; i < 9; i++)
+    {
+        if(valid != 1)
+        {
+            colorBoard[row][i] = valid;
+        }
+    }
+
+    console.log(colorBoard);
+    
+}
+
+function updateColorBoardCol(col, valid)
+{
+    for(let i = 0; i < 9; i++)
+    {
+        if(valid != 1)
+        {
+            colorBoard[i][col] = valid;
+        }
+    }
+    console.log(colorBoard);
+}
+
+function updateColorBoardBox(startRow, startCol, valid)
+{
+    for(let i = startRow; i <  startRow + 3; i++)
+    {
+        for(let k = startCol; k < startCol + 3; k++)
+        {
+            colorBoard[i][k] = valid;
+        }
+    }
+}
+
+function updateColorBoardCell(row, col, valid)
+{
+    colorBoard[row][col] = valid;
+}
+
+function updateColorBoardCross(valid)
+{
+    for(let i = 0; i < 9; i++)
+    {
+        for(let k = 0; k < 9; k++)
+        {
+            colorBoard[i][i] = valid;
+            colorBoard[8 - i][i] =  valid;
+        }
+    }
+}
+
+async function updateColorBoard()
+{
+    for(let i = 0; i < 9; i++)
+    {
+        for(let k = 0; k < 9; k++)
+        {
+            let square = document.getElementById((9 * i + k + 1).toString());
+
+            if(colorBoard[i][k] === 1)
+            {
+                square.style.backgroundColor = "green";
+            }
+            else
+            {
+                square.style.backgroundColor = "red";
+            }
+
+            await sleep(shortDelay);
+        }
+    }
+
+    console.log("colored board");
+    
+}
+
+async function reset()
+{
+    for(let i = 8; i >= 0; i--)
+    {
+        for(let k = 8; k >= 0; k--)
+        {
+            colorBoard[i][k] = 1;
+            let square = document.getElementById((9 * i + k + 1).toString());
+            square.style.backgroundColor = "#aaaaaa";
+            await sleep(shortDelay);
+        }
+    }
+
     
 }
 
@@ -263,10 +576,12 @@ initializeConstBoard();
 
 createBoard();
 
-validSudoku();
+// validSudoku();
 
-validKnightsMove();
+// validKnightsMove();
 
-validKingsMove();
+// validKingsMove();
 
-validCross();
+// validCross();
+
+// colorRow();
